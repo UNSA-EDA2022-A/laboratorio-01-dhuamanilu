429e742063dbe435fa58bb4788fa80028bfe7d58
dhuamanilu <87215556+dhuamanilu@users.noreply.github.com>
2022-05-04 12:28:43 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -15,8 +15,29 @@ public class Exercise1 {
 	}
 
 	public String convertirBaseGabriel(int a) {
-
-		// TO DO
-		return "El numero proporcionado no esta en base Gabriel.";
+		int guarda=a;
+		String num = Integer.toString(a);
+		int k=0;
+		while(k<num.length()) {
+			if(num.charAt(k)=='2') {
+				int j=k+1;
+				while(j<num.length()) {
+					if(num.charAt(j)!='0') {
+						return "El numero proporcionado no esta en base Gabriel.";
+					}
+					j++;
+				}
+			}	
+			k++;
+		}
+		int rpta=0,i=0;
+		while(guarda!=0) {
+			int digito=guarda%10;
+			rpta+=digito*(Math.pow(2,i+1)-1);
+			i++;
+			guarda/=10;
+		}
+		
+		return rpta+"";
 	}
 }
